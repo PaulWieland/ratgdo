03c7279784071f49c5d425d759f0f16e6b6f596f
Paul Wieland <p@ul.sickdimension.com>
2023-02-02 14:03:10 -0500

    Version 1.7

--- src/ratgdo.cpp
+++ src/ratgdo.cpp
@@ -452,7 +452,6 @@ void callback(char *topic, byte *payload, unsigned int length){
 
 	if(strcmp(topic,doorCommandTopic.c_str()) == 0){
 		doorCommand = (String)json[VALUE];
-		Serial.println(doorCommand);
 
 		if (doorCommand == "open"){
 			Serial.println("MQTT: open the door");
